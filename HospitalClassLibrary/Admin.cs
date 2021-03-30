c99ebb95177634f842c3550135df04375faec4f3
k-wiese <75091948+k-wiese@users.noreply.github.com>
2021-03-30 19:15:02 +0200

    Create Admin.cs

---
+++ HospitalClassLibrary/Admin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalClassLibrary
+{
+    [Serializable]
+    //klasa administratora
+    public class Admin : Employee
+    {
+        public Admin(string imie, string nazwisko, int[] pesel, string nazwaUzytkownika, string haslo) : base(imie, nazwisko, pesel, nazwaUzytkownika, haslo)
+        {
+
+        }
+        //funkcja info po polimorfizacji dla Admin
+        public override string Info()
+        {
+            return base.Info() + "Admin";
+
+        }
+
+    }
+}
