1e566df03dec2cd0558f8d5f3df6e8eabb8ade54
k-wiese <75091948+k-wiese@users.noreply.github.com>
2021-03-30 19:06:21 +0200

    Create Nurse.cs

---
+++ HospitalClassLibrary/Nurse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace HospitalClassLibrary
+{
+    [Serializable]
+    
+    public class Nurse : Employee
+    {
+        //przeładowany konstruktor dla pielęgniarki
+        public Nurse(string name, string surname, int[] pesel, string username, string password) : base(name, surname, pesel, username, password)
+        {
+        }
+        //funkcja info po polimorfizacji dla pielęgniarki
+        public override string Info()
+        {
+            return base.Info() + "Pielęgniarka";
+
+        }
+
+        private string GetDebuggerDisplay()
+        {
+            return ToString();
+        }
+    }
+}
