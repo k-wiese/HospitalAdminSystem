f383cfc167970266433f00e6f386d68187d38f88
k-wiese <75091948+k-wiese@users.noreply.github.com>
2021-03-30 19:06:27 +0200

    Create Doctor.cs

---
+++ HospitalClassLibrary/Doctor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalClassLibrary
+{
+    [Serializable]
+
+    public class Doctor : Employee
+    {
+        //członkowie klasy doctor
+        string specialization;
+        public string Specialization
+        {
+            get { return specialization; }
+            set { specialization = value; }
+        }
+        int[] PWZnumber = new int[7];
+
+        //funkcja info po polimorfizacji dla doctor
+        public override string Info()
+        {
+            return base.Info() + "Lekarz " + Specialization;
+        }
+
+        //przeładowany konstruktor dla doctor
+        public Doctor(string name, string surname, int[] pesel, string username, string password, string specialization, int[] PWZnumber) : base(name, surname, pesel, username, password)
+        {
+            this.specialization = specialization;
+            this.PWZnumber = PWZnumber;
+        }
+        //przeładowana funkcja zmiany danych
+        public void ChangeUserData(string name, string surname, int[] pesel, string username, string password, string specialization, int[] PWZnumber)
+        {
+            base.ChangeUserData(name, surname, pesel, username, password);
+            this.specialization = specialization;
+            this.PWZnumber = PWZnumber;
+
+        }
+
+    }
+}
